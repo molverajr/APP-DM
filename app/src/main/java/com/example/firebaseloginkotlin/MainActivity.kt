65063882c353ed1768e0ab96ecff4108de46d856
Dapier <josuea501@gmail.com>
2022-09-30 10:24:46 -0500

    Splash screen agregada

--- app/src/main/java/com/example/firebaseloginkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firebaseloginkotlin/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
